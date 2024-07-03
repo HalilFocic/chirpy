ac3be6b83a57f08b7dce1e1e7fcb013e1c553b3d
Halil Focic <halil.focic@zendev.se>
2024-07-03 17:52:01 +0200

    feat: added FileServer handle to ServeMux

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+    "log"
+)
+
+func main() {
+    const port = "8080"
+
+    mux := http.NewServeMux()
+    mux.Handle("/",http.FileServer(http.Dir(".")))
+    srv := &http.Server{
+        Addr: ":" + port,
+        Handler: mux,
+    }
+    log.Printf("Serving on port: %s\n",port)
+    log.Fatal(srv.ListenAndServe())
+}
